49a74f81aea979185590241d9bfc90de963751a3
SaiCharan <charansai612@gmail.com>
2017-07-25 21:02:47 +0530

    land: Enable Camera HAL3

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
     libshims_camera \
     Snap
 
-# ZRAM
+# Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
     media.camera.ts.monotonic=0 \
@@ -13,4 +13,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.dcvs.extra-buff-count=2 \
     vidc.enc.disable.pq=true \
     media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
+    media.stagefright.less-secure=true \
+    persist.camera.HAL3.enabled=1
