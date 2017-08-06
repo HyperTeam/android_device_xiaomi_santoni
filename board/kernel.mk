7b3e2c7ee584c570c81621dacd20827885ca22f4
IsaacChen <tingyi364@gmail.com>
2017-08-06 23:25:26 +0800

    santoni: Add prebuilt modules
    Signed-off-by: IsaacChen <tingyi364@gmail.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -11,3 +11,20 @@ TARGET_KERNEL_CONFIG := lineageos_santoni_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 TARGET_USE_SDCLANG := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+
+# Modules
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/prebuilt/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
+    $(DEVICE_PATH)/prebuilt/modules/backlight.ko:system/lib/modules/backlight.ko \
+    $(DEVICE_PATH)/prebuilt/modules/br_netfilter.ko:system/lib/modules/br_netfilter.ko \
+    $(DEVICE_PATH)/prebuilt/modules/evbug.ko:system/lib/modules/evbug.ko \
+    $(DEVICE_PATH)/prebuilt/modules/generic_bl.ko:system/lib/modules/generic_bl.ko \
+    $(DEVICE_PATH)/prebuilt/modules/lcd.ko:system/lib/modules/lcd.ko \
+    $(DEVICE_PATH)/prebuilt/modules/mmc_block_test.ko:system/lib/modules/mmc_block_test.ko \
+    $(DEVICE_PATH)/prebuilt/modules/mmc_test.ko:system/lib/modules/mmc_test.ko \
+    $(DEVICE_PATH)/prebuilt/modules/rdbg.ko:system/lib/modules/rdbg.ko \
+    $(DEVICE_PATH)/prebuilt/modules/spidev.ko:system/lib/modules/spidev.ko \
+    $(DEVICE_PATH)/prebuilt/modules/test-iosched.ko:system/lib/modules/test-iosched.ko \
+    $(DEVICE_PATH)/prebuilt/modules/ufs_test.ko:system/lib/modules/ufs_test.ko \
+    $(DEVICE_PATH)/prebuilt/modules/wil6210.ko:system/lib/modules/wil6210.ko \
+    $(DEVICE_PATH)/prebuilt/modules/wlan.ko:system/lib/modules/wlan.ko
