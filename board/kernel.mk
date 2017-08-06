b06ae87cf6f93ffb725371e4433a2beb7bd28383
Isaac Chen <tingyi364@gmail.com>
2017-08-06 23:35:49 +0800

    santoni: Use land source for now

--- board/kernel.mk
+++ board/kernel.mk
@@ -7,7 +7,7 @@ BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := lineageos_santoni_defconfig
+TARGET_KERNEL_CONFIG := lineageos_land_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 TARGET_USE_SDCLANG := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
